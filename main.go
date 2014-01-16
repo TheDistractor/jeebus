3bc2f0c9ba28b42d467346b46f597f1422de3858
Jean-Claude Wippler <jc@wippler.nl>
2014-01-16 13:41:59 +0100

    start adding basic web demo

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	println("hello")
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir("public")))
+
+	println("listening on port 3333")
+	log.Fatal(http.ListenAndServe("localhost:3333", mux))
 }
