887abcb9d02b337fd9fb52455a1cf2528de602b5
Jean-Claude Wippler <jc@wippler.nl>
2014-02-23 20:27:12 +0100

    add tick subcommand

--- tools.go
+++ tools.go
@@ -2,6 +2,7 @@ package jeebus
 
 import (
 	"log"
+	"time"
 
 	"github.com/codegangsta/cli"
 )
@@ -9,11 +10,7 @@ import (
 func DefineToolCommands() {
 	noCustomSubCommands := app == nil || len(app.Commands) == 0
 
-	cmd := AddCommand("run", func(c *cli.Context) {
-		OpenDatabase()
-		StartMessaging()
-		RunHttpServer()
-	})
+	cmd := AddCommand("run", RunCmd)
 	cmd.ShortName = "r"
 	cmd.Usage = "launch the web server with messaging and database"
 
@@ -24,6 +21,10 @@ func DefineToolCommands() {
 	cmd = AddCommand("publish", PublishCmd)
 	cmd.ShortName = "p"
 	cmd.Usage = "publish a message to a topic"
+
+	cmd = AddCommand("tick", TickCmd)
+	cmd.ShortName = "t"
+	cmd.Usage = "generate a periodic tick message for testing"
 
 	cmd = AddCommand("dump", DumpCmd)
 	cmd.ShortName = "d"
@@ -43,6 +44,12 @@ func DefineToolCommands() {
 	}
 }
 
+func RunCmd(c *cli.Context) {
+	OpenDatabase()
+	StartMessaging()
+	RunHttpServer()
+}
+
 func SubscribeCmd(c *cli.Context) {
 	pattern := c.Args().First()
 	if pattern == "" {
@@ -59,6 +66,21 @@ func (s *SubscribeService) Handle(topic string, payload []byte) {
 }
 
 func PublishCmd(c *cli.Context) {
+}
+
+func TickCmd(c *cli.Context) {
+	topic := c.Args().First()
+	if topic == "" {
+		topic = "/jb/tick"
+	}
+	done := StartClient()
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		for tick := range ticker.C {
+			Publish(topic, tick.String())
+		}
+	}()
+	<- done
 }
 
 func DumpCmd(c *cli.Context) {
