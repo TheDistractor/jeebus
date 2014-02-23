7342a03da32f1fef5370fd18e630348fe8c563e1
Jean-Claude Wippler <jc@wippler.nl>
2014-02-23 21:26:12 +0100

    implement the rest of the sub-commands

--- tools.go
+++ tools.go
@@ -1,6 +1,9 @@
 package jeebus
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"fmt"
 	"log"
 	"time"
 
@@ -10,7 +13,11 @@ import (
 func DefineToolCommands() {
 	noCustomSubCommands := app == nil || len(app.Commands) == 0
 
-	cmd := AddCommand("run", RunCmd)
+	cmd := AddCommand("run", func(c *cli.Context) {
+		OpenDatabase()
+		StartMessaging()
+		RunHttpServer()
+	})
 	cmd.ShortName = "r"
 	cmd.Usage = "launch the web server with messaging and database"
 
@@ -30,11 +37,23 @@ func DefineToolCommands() {
 	cmd.ShortName = "d"
 	cmd.Usage = "dump the database contents (offline)"
 
-	cmd = AddCommand("import", ImportCmd)
+	cmd = AddCommand("import", func(c *cli.Context) {
+		if len(c.Args()) < 1 {
+			log.Fatalf("usage: jb import <jsonfile>")
+		}
+		OpenDatabase()
+		ImportJsonData(c.Args().First())
+	})
 	cmd.ShortName = "i"
 	cmd.Usage = "import a JSON file into the database (offline)"
 
-	cmd = AddCommand("export", ExportCmd)
+	cmd = AddCommand("export", func(c *cli.Context) {
+		if len(c.Args()) < 1 {
+			log.Fatalf("usage: jb export <prefix>")
+		}
+		OpenDatabase()
+		ExportJsonData(c.Args().First())
+	})
 	cmd.ShortName = "e"
 	cmd.Usage = "export the database as JSON (offline)"
 
@@ -42,12 +61,6 @@ func DefineToolCommands() {
 		// also run by default when no other commands have been defined
 		app.Action = app.Command("run").Action
 	}
-}
-
-func RunCmd(c *cli.Context) {
-	OpenDatabase()
-	StartMessaging()
-	RunHttpServer()
 }
 
 func SubscribeCmd(c *cli.Context) {
@@ -66,6 +79,14 @@ func (s *SubscribeService) Handle(topic string, payload []byte) {
 }
 
 func PublishCmd(c *cli.Context) {
+	if len(c.Args()) < 2 {
+		log.Fatalf("usage: jb pub <topic> ?<jsonval>?")
+	}
+	topic, value := c.Args().Get(0), c.Args().Get(1)
+	StartClient()
+	Publish(topic, []byte(value))
+	// TODO: need to close gracefully, and not too soon!
+	time.Sleep(10 * time.Millisecond)
 }
 
 func TickCmd(c *cli.Context) {
@@ -84,10 +105,92 @@ func TickCmd(c *cli.Context) {
 }
 
 func DumpCmd(c *cli.Context) {
+	from, to := c.Args().Get(0), c.Args().Get(1)
+	if to == "" {
+		to = from + "~" // FIXME this assumes all key chars are less than "~"
+	}
+
+	OpenDatabase()
+	// get and print all the key/value pairs from the database
+	iter := db.NewIterator(nil)
+	iter.Seek([]byte(from))
+	for iter.Valid() {
+		if string(iter.Key()) > to {
+			break
+		}
+		fmt.Printf("%s = %s\n", iter.Key(), iter.Value())
+		if !iter.Next() {
+			break
+		}
+	}
+	iter.Release()
 }
 
-func ImportCmd(c *cli.Context) {
+func ImportJsonData(filename string) {
+	data, err := ioutil.ReadFile(filename)
+	Check(err)
+
+	var values map[string]map[string]*json.RawMessage
+	err = json.Unmarshal(data, &values)
+	Check(err)
+
+	for prefix, entries := range values {
+		limit := prefix + "~" // FIXME see below, same as for dumpDatabase()
+		var ndel, nadd int
+
+		// get and print all the key/value pairs from the database
+		iter := db.NewIterator(nil)
+		iter.Seek([]byte(prefix))
+		for iter.Valid() {
+			key := string(iter.Key())
+			if key > limit {
+				break
+			}
+			err = db.Delete([]byte(key), nil)
+			Check(err)
+			ndel++
+			if !iter.Next() {
+				break
+			}
+		}
+		iter.Release()
+
+		for k, v := range entries {
+			err = db.Put([]byte(prefix+k), *v, nil)
+			Check(err)
+			nadd++
+		}
+
+		fmt.Printf("%d deleted, %d added for prefix %q\n", ndel, nadd, prefix)
+	}
 }
 
-func ExportCmd(c *cli.Context) {
+func ExportJsonData(prefix string) {
+	limit := prefix + "~" // FIXME see below, same as for dumpDatabase()
+	entries := make(map[string]interface{})
+
+	// get and print all the key/value pairs from the database
+	iter := db.NewIterator(nil)
+	iter.Seek([]byte(prefix))
+	for iter.Valid() {
+		key := iter.Key()[len(prefix):]
+		if string(iter.Key()) > limit {
+			break
+		}
+		var value interface{}
+		err := json.Unmarshal(iter.Value(), &value)
+		Check(err)
+		entries[string(key)] = value
+		if !iter.Next() {
+			break
+		}
+	}
+	iter.Release()
+
+	values := make(map[string]map[string]interface{})
+	values[prefix] = entries
+
+	s, e := json.MarshalIndent(values, "", "  ")
+	Check(e)
+	fmt.Println(string(s))
 }
