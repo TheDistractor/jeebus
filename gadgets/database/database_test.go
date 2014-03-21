d1768356718aba16bb1455f6dedfa3e9ee95be31
Jean-Claude Wippler <jc@wippler.nl>
2014-03-21 17:41:47 +0100

    fix test regression
    sending some output to Printer causes a timing sensitivity

--- gadgets/database/database_test.go
+++ gadgets/database/database_test.go
@@ -18,8 +18,6 @@ func init() {
 func ExampleLevelDB() {
 	g := flow.NewCircuit()
 	g.Add("db", "LevelDB")
-	g.Add("p", "Printer")
-	g.Connect("db.Mods", "p.In", 0)
 	g.Feed("db.Name", testDb)
 	g.Feed("db.In", flow.Tag{"a/b", "123"})
 	g.Feed("db.In", flow.Tag{"a/c", "456"})
@@ -31,18 +29,18 @@ func ExampleLevelDB() {
 	g.Feed("db.In", flow.Tag{"a/c", nil})
 	g.Run()
 	// Output:
-	// {Tag:a/b Msg:123}
-	// {Tag:a/c Msg:456}
+	// Lost flow.Tag: {a/b 123}
+	// Lost flow.Tag: {a/c 456}
 	// Lost flow.Tag: {<get> a/b}
 	// Lost string: 123
 	// Lost flow.Tag: {<keys> a/}
 	// Lost []string: [b c]
+	// Lost flow.Tag: {a/b <nil>}
 	// Lost flow.Tag: {<get> a/b}
 	// Lost <nil>: <nil>
 	// Lost flow.Tag: {<keys> a/}
 	// Lost []string: [c]
-	// {Tag:a/b Msg:<nil>}
-	// {Tag:a/c Msg:<nil>}
+	// Lost flow.Tag: {a/c <nil>}
 }
 
 func TestDatabase(t *testing.T) {
